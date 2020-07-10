1427c29a4b32f6413224b8780541d90157e793aa
Matthew <matthewmarkmcc@gmail.com>
2020-07-10 12:56:22 +1000

    Speed update to stepper motor

--- RPiStepperMotorControl.py
+++ RPiStepperMotorControl.py
@@ -6,12 +6,12 @@ out2 = 11
 out3 = 15
 out4 = 12
 
-i=0
-positive=0
-negative=0
-y=0
+i = 0
+positive = 0
+negative = 0
+y = 0
 
-
+sleepDelay = .01
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(out1,GPIO.OUT)
@@ -24,87 +24,90 @@ print "First calibrate by giving some +ve and -ve values....."
 
 try:
    while(1):
-      GPIO.output(out1,GPIO.LOW)
-      GPIO.output(out2,GPIO.LOW)
-      GPIO.output(out3,GPIO.LOW)
-      GPIO.output(out4,GPIO.LOW)
-      x = input()
-      if x>0 and x<=400:
-          for y in range(x,0,-1):
-              if negative==1:
-                  if i==7:
-                      i=0
+      GPIO.output(out1, GPIO.LOW)
+      GPIO.output(out2, GPIO.LOW)
+      GPIO.output(out3, GPIO.LOW)
+      GPIO.output(out4, GPIO.LOW)
+
+      stepsToMove = input()
+      sleepDelay = input()
+
+      if stepsToMove > 0 and stepsToMove <= 400:
+          for y in range(stepsToMove, 0, -1):
+              if negative == 1:
+                  if i == 7:
+                      i = 0
                   else:
-                      i=i+1
-                  y=y+2
-                  negative=0
-              positive=1
-              #print((x+1)-y)
-              if i==0:
-                  GPIO.output(out1,GPIO.HIGH)
-                  GPIO.output(out2,GPIO.LOW)
-                  GPIO.output(out3,GPIO.LOW)
-                  GPIO.output(out4,GPIO.LOW)
-                  time.sleep(0.03)
+                      i = i + 1
+                  y = y + 2
+                  negative = 0
+              positive = 1
+              #print((stepsToMove+1)-y)
+              if i == 0:
+                  GPIO.output(out1, GPIO.HIGH)
+                  GPIO.output(out2, GPIO.LOW)
+                  GPIO.output(out3, GPIO.LOW)
+                  GPIO.output(out4, GPIO.LOW)
+                  time.sleep(sleepDelay)
                   #time.sleep(1)
-              elif i==1:
-                  GPIO.output(out1,GPIO.HIGH)
-                  GPIO.output(out2,GPIO.HIGH)
-                  GPIO.output(out3,GPIO.LOW)
-                  GPIO.output(out4,GPIO.LOW)
-                  time.sleep(0.03)
+              elif i == 1:
+                  GPIO.output(out1, GPIO.HIGH)
+                  GPIO.output(out2, GPIO.HIGH)
+                  GPIO.output(out3, GPIO.LOW)
+                  GPIO.output(out4, GPIO.LOW)
+                  time.sleep(sleepDelay)
                   #time.sleep(1)
-              elif i==2:  
-                  GPIO.output(out1,GPIO.LOW)
-                  GPIO.output(out2,GPIO.HIGH)
-                  GPIO.output(out3,GPIO.LOW)
-                  GPIO.output(out4,GPIO.LOW)
-                  time.sleep(0.03)
+              elif i == 2:  
+                  GPIO.output(out1, GPIO.LOW)
+                  GPIO.output(out2, GPIO.HIGH)
+                  GPIO.output(out3, GPIO.LOW)
+                  GPIO.output(out4, GPIO.LOW)
+                  time.sleep(sleepDelay)
                   #time.sleep(1)
-              elif i==3:    
-                  GPIO.output(out1,GPIO.LOW)
-                  GPIO.output(out2,GPIO.HIGH)
-                  GPIO.output(out3,GPIO.HIGH)
-                  GPIO.output(out4,GPIO.LOW)
-                  time.sleep(0.03)
+              elif i == 3:    
+                  GPIO.output(out1, GPIO.LOW)
+                  GPIO.output(out2, GPIO.HIGH)
+                  GPIO.output(out3, GPIO.HIGH)
+                  GPIO.output(out4, GPIO.LOW)
+                  time.sleep(sleepDelay)
                   #time.sleep(1)
-              elif i==4:  
-                  GPIO.output(out1,GPIO.LOW)
-                  GPIO.output(out2,GPIO.LOW)
-                  GPIO.output(out3,GPIO.HIGH)
-                  GPIO.output(out4,GPIO.LOW)
-                  time.sleep(0.03)
+              elif i == 4:  
+                  GPIO.output(out1, GPIO.LOW)
+                  GPIO.output(out2, GPIO.LOW)
+                  GPIO.output(out3, GPIO.HIGH)
+                  GPIO.output(out4, GPIO.LOW)
+                  time.sleep(sleepDelay)
                   #time.sleep(1)
-              elif i==5:
-                  GPIO.output(out1,GPIO.LOW)
-                  GPIO.output(out2,GPIO.LOW)
-                  GPIO.output(out3,GPIO.HIGH)
-                  GPIO.output(out4,GPIO.HIGH)
-                  time.sleep(0.03)
+              elif i == 5:
+                  GPIO.output(out1, GPIO.LOW)
+                  GPIO.output(out2, GPIO.LOW)
+                  GPIO.output(out3, GPIO.HIGH)
+                  GPIO.output(out4, GPIO.HIGH)
+                  time.sleep(sleepDelay)
                   #time.sleep(1)
-              elif i==6:    
-                  GPIO.output(out1,GPIO.LOW)
-                  GPIO.output(out2,GPIO.LOW)
-                  GPIO.output(out3,GPIO.LOW)
-                  GPIO.output(out4,GPIO.HIGH)
-                  time.sleep(0.03)
+              elif i == 6:    
+                  GPIO.output(out1, GPIO.LOW)
+                  GPIO.output(out2, GPIO.LOW)
+                  GPIO.output(out3, GPIO.LOW)
+                  GPIO.output(out4, GPIO.HIGH)
+                  time.sleep(sleepDelay)
                   #time.sleep(1)
-              elif i==7:    
-                  GPIO.output(out1,GPIO.HIGH)
-                  GPIO.output(out2,GPIO.LOW)
-                  GPIO.output(out3,GPIO.LOW)
-                  GPIO.output(out4,GPIO.HIGH)
-                  time.sleep(0.03)
+              elif i == 7:    
+                  GPIO.output(out1, GPIO.HIGH)
+                  GPIO.output(out2, GPIO.LOW)
+                  GPIO.output(out3, GPIO.LOW)
+                  GPIO.output(out4, GPIO.HIGH)
+                  time.sleep(sleepDelay)
                   #time.sleep(1)
-              if i==7:
-                  i=0
+              if i == 7:
+                  i = 0
                   continue
-              i=i+1
+              i = i + 1
       
       
-      elif x<0 and x>=-400:
-          x=x*-1
-          for y in range(x,0,-1):
+      elif stepsToMove < 0 and stepsToMove >= -400:
+          stepsToMove=stepsToMove*-1
+          for y in range(stepsToMove,0,-1):
               if positive==1:
                   if i==0:
                       i=7
@@ -113,7 +116,7 @@ try:
                   y=y+3
                   positive=0
               negative=1
-              #print((x+1)-y) 
+              #print((stepsToMove+1)-y) 
               if i==0:
                   GPIO.output(out1,GPIO.HIGH)
                   GPIO.output(out2,GPIO.LOW)
